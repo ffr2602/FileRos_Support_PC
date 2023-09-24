72e039512bf72756fb15a6235717fbe2dce6569b
Fahmi Fathur Rohman <100126219+ffr2602@users.noreply.github.com>
2023-09-24 22:19:30 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 read -p "Instalasi Package ROS2: " ros;
-sudo apt install ros-$ros-gazebo-ros-pkgs
-sudo apt install ros-$ros-nav2-bringup
-sudo apt install ros-$ros-navigation2
-sudo apt install ros-$ros-turtlebot3*
-sudo apt install ros-$ros-nav2-simple-commander
-sudo apt install ros-$ros-slam-toolbox
 sudo apt install ros-$ros-xacro
-sudo apt install ros-$ros-joint-state-publisher-gui
-sudo apt install ros-$ros-ros2-control
-sudo apt install ros-$ros-ros2-controllers
+sudo apt install ros-$ros-joint-state-publisher-gui -y
+sudo apt install ros-$ros-gazebo-ros-pkgs -y
 sudo apt install ros-$ros-gazebo-ros2-control
-sudo apt install ros-$ros-robot-localization
-sudo apt install ros-$ros-rqt-robot-steering
-sudo apt install python3-serial
+sudo apt install ros-$ros-ros2-control -y
+sudo apt install ros-$ros-ros2-controllers -y
+sudo apt install ros-$ros-twist-mux
 sudo apt install ros-$ros-rplidar-ros
+sudo apt install ros-$ros-nav2-bringup -y
+sudo apt install ros-$ros-navigation2
+sudo apt install ros-$ros-turtlebot3* -y
+sudo apt install ros-$ros-slam-toolbox -y
+sudo apt install ros-$ros-robot-localization -y
 sudo apt install ros-$ros-image-transport-plugins
 sudo apt install ros-$ros-rqt-image-view
+sudo apt install joystick jstest-gtk evtest
+sudo apt install python3-serial
