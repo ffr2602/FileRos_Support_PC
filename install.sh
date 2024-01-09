12f310c5160c62af6b94da0e2876ff99f0bc3f6a
Fahmi Fathur Rohman <100126219+ffr2602@users.noreply.github.com>
2024-01-09 23:51:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 read -p "Instalasi Package ROS2: " ros;
 sudo apt install ros-$ros-xacro
+sudo apt install ros-$ros-tf-transformations -y
 sudo apt install ros-$ros-joint-state-publisher-gui -y
 sudo apt install ros-$ros-gazebo-ros-pkgs -y
 sudo apt install ros-$ros-gazebo-ros2-control -y
@@ -12,7 +13,11 @@ sudo apt install ros-$ros-nav2-bringup -y
 sudo apt install ros-$ros-navigation2 -y
 sudo apt install ros-$ros-slam-toolbox -y
 sudo apt install ros-$ros-robot-localization -y
-sudo apt install ros-$ros-image-transport-plugins -y 
-sudo apt install ros-$ros-rqt-image-view -y 
+sudo apt install ros-$ros-bno055 -y
+sudo apt install ros-$ros-laser-filters -y
 sudo apt install joystick jstest-gtk evtest -y
+sudo apt install python3-pip -y
+sudo apt install python3-smbus -y
 sudo apt install python3-serial
+sudo pip install setuptools==58.2
+sudo pip install transform3d
