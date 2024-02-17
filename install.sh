61a535143f82352c38146001b65d6c69def825e8
Fahmi Fathur Rohman <100126219+ffr2602@users.noreply.github.com>
2024-02-18 00:29:33 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,19 @@
-#!/bin/sh
-read -p "Instalasi Package ROS2: " ros;
-sudo apt install ros-$ros-xacro
-sudo apt install ros-$ros-tf-transformations -y
-sudo apt install ros-$ros-joint-state-publisher-gui -y
-sudo apt install ros-$ros-gazebo-ros-pkgs -y
-sudo apt install ros-$ros-gazebo-ros2-control -y
-sudo apt install ros-$ros-ros2-control -y
-sudo apt install ros-$ros-ros2-controllers -y
-sudo apt install ros-$ros-twist-mux -y
-sudo apt install ros-$ros-rplidar-ros -y
-sudo apt install ros-$ros-nav2-bringup -y
-sudo apt install ros-$ros-navigation2 -y
-sudo apt install ros-$ros-slam-toolbox -y
-sudo apt install ros-$ros-robot-localization -y
-sudo apt install ros-$ros-bno055 -y
-sudo apt install ros-$ros-laser-filters -y
+# file tambahan --ROS
+sudo apt install ros-$ROS_DISTRO-xacro
+sudo apt install ros-$ROS_DISTRO-tf-transformations -y
+sudo apt install ros-$ROS_DISTRO-joint-state-publisher-gui -y
+sudo apt install ros-$ROS_DISTRO-gazebo-ros-pkgs -y
+sudo apt install ros-$ROS_DISTRO-gazebo-ros2-control -y
+sudo apt install ros-$ROS_DISTRO-ros2-control -y
+sudo apt install ros-$ROS_DISTRO-ros2-controllers -y
+sudo apt install ros-$ROS_DISTRO-twist-mux -y
+sudo apt install ros-$ROS_DISTRO-rplidar-ros -y
+sudo apt install ros-$ROS_DISTRO-nav2-bringup -y
+sudo apt install ros-$ROS_DISTRO-navigation2 -y
+sudo apt install ros-$ROS_DISTRO-slam-toolbox -y
+sudo apt install ros-$ROS_DISTRO-robot-localization -y
+sudo apt install ros-$ROS_DISTRO-bno055 -y
+sudo apt install ros-$ROS_DISTRO-laser-filters -y
 sudo apt install joystick jstest-gtk evtest -y
 sudo apt install python3-pip -y
 sudo apt install python3-smbus -y
